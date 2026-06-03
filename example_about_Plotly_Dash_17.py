7c0d7ff8b7e449bc17d39a622b742bc497a83d05
IvanSlivenko <ivan8822@ukr.net>
2026-06-03 14:17:50 +0300

    03-06-2026-1

--- example_about_Plotly_Dash_17.py
+++ example_about_Plotly_Dash_17.py
@@ -36,7 +36,6 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    # app.run_server(debug=True)
     app.run(debug=True)
 
 
---
+++ example_about_Plotly_Dash_18.py
@@ -0,0 +1,47 @@
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+
+df = px.data.gapminder()
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Graphic App"),
+    html.Label("Оберіть країну"),
+    dcc.Dropdown(
+        id='country-dropdown',
+        options=[{'label': country, 'value': country} for country in df['country'].unique()],
+        value='Canada'
+    ),
+    html.Label("Оберіть рік"),
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        step=None
+    ),
+    dcc.Graph(id='indicator-graphic')
+])
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('country-dropdown', 'value'),
+    Input('year-slider', 'value')
+)
+
+def update_graph(selected_country, selected_year):
+    filtered = df[(df['country'] == selected_country) & (df['year'] == selected_year)]
+
+    fig = px.bar(filtered, x='country',  y='gdpPercap', hover_data=['pop', 'lifeExp'], color='lifeExp', labels={'gdpPercap': 'GDP'},height=400)
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
